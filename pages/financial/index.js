a12521b13554f2c3dfeb6b2c2ecec50905e1bbb4
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2022-08-21 09:01:19 +0530

    Non Responsive Solution

---
+++ pages/financial/index.js
@@ -0,0 +1,24 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd7c30b3f69msh79cfc37cf2576b4p17e14ajsnf60a516795b8',
+		'X-RapidAPI-Host': 'free-news.p.rapidapi.com'
+	}
+};
+
+fetch('https://free-news.p.rapidapi.com/v1/search?q=Indian%20Finance&lang=en', options)
+	.then(response => response.json())
+	.then(
+        (data)=>{
+
+            document.getElementById('one').innerHTML=data.articles[1].title;
+			document.getElementById('three').innerHTML=data.articles[3].title;
+			document.getElementById('four').innerHTML=data.articles[4].title;
+			document.getElementById('two').innerHTML=data.articles[2].title;
+			document.getElementById('zero_media').setAttribute('src',data.articles[0].media);
+            document.getElementById('zero_title').innerHTML=data.articles[0].title;
+			document.getElementById('zero_summary').innerHTML=data.articles[0].summary;
+			console.log(data);
+        }
+    )
+	.catch(err => console.error(err));
